9dcaff7281c11557556ddb7d8e34f6d0808fdb36
phatvphat <phatvphat@gmail.com>
2021-09-21 13:41:53 +0700

    update index

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const yas = require('youtube-audio-server')
 // Start listener (REST API).
 const port = process.env.PORT || 7331;
 yas.listen(port, () => {
-	console.log(`Listening on port http://localhost:${port}.`)
+	console.log(`Listening on port http://localhost:${port}.`);
 })
